8c78f1ae7e09e74e28fc53122779b421ebbd1473
Bekircan DAL <bekircandal.amasya.05@gmail.com>
2018-07-27 15:58:32 +0300

    Added fixes for some conditions.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -75,29 +75,74 @@ void MainWindow::on_actionNew_triggered()
 
 void MainWindow::on_actionOpen_triggered()
 {
-    QString fileName = QFileDialog::getOpenFileName(this, "Open the file");
-    QFile file(fileName);
-    currentFile = fileName;
 
-    if (!file.open(QIODevice::ReadOnly | QFile::Text)) {
+    if(textChanged){
+        QMessageBox messageBox;
+        messageBox.setIcon(QMessageBox::Question);
+        messageBox.setText("The document has been modified.");
+        messageBox.setInformativeText("Do you want to save your changes?");
+        messageBox.setStandardButtons(QMessageBox::Save | QMessageBox::Discard | QMessageBox::Cancel);
+        messageBox.setDefaultButton(QMessageBox::Save);
+        int ret = messageBox.exec();
 
-        QMessageBox::warning(this, "Warning", "Cannot open file" + file.errorString());
-        return;
+        switch (ret) {
+          case QMessageBox::Save:
+                // Save was clicked
+
+            if (isNewFile == true) {
+                //Save as new file
+                MainWindow::on_actionSave_As_triggered();
+            }else{
+                //Overwrite the file
+                MainWindow::on_actionSave_triggered();
+            }
+
+              isNewFile = false;
+              textChanged = false;
+
+              MainWindow::on_actionOpen_triggered();
+
+              break;
+          case QMessageBox::Discard:
+              // Don't Save was clicked
+              isNewFile = false;
+              textChanged = false;
+
+              MainWindow::on_actionOpen_triggered();
+              break;
+          case QMessageBox::Cancel:
+              // Cancel was clicked
+              messageBox.close();
+              break;
+          default:
+              // should never be reached
+              break;
+        }
+    }else{
+
+        QString fileName = QFileDialog::getOpenFileName(this, "Open the file");
+        QFile file(fileName);
+        currentFile = fileName;
+
+        if (!file.open(QIODevice::ReadOnly | QFile::Text)) {
+
+            QMessageBox::warning(this, "Warning", "Cannot open file" + file.errorString());
+            return;
+        }
+
+        setWindowTitle(fileName);
+        QTextStream in(&file);
+        QString text = in.readAll();
+        ui->textEdit->setText(text);
+        file.close();
+
+        ui->statusBar->showMessage("Line count : " + QString::number(ui->textEdit->document()->blockCount()));
+        isNewFile = false;
+        textChanged = false;
+
+        ui->actionSave->setDisabled(true);
+        ui->actionSave_As->setEnabled(true);
     }
-
-    setWindowTitle(fileName);
-    QTextStream in(&file);
-    QString text = in.readAll();
-    ui->textEdit->setText(text);
-    file.close();
-
-    ui->statusBar->showMessage("Line count : " + QString::number(ui->textEdit->document()->blockCount()));
-    isNewFile = false;
-    textChanged = false;
-
-    ui->actionSave->setDisabled(true);
-    ui->actionSave_As->setEnabled(true);
-
 }
 
 void MainWindow::on_actionSave_As_triggered()
